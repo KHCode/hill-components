e918bfa00eafcc21834aa181cc521c937748b9ca
KHCode <hillkri@oregonstate.edu>
2022-07-03 00:41:06 -0700

    add some flex styles to navbar

--- src/components/hill-navbar/hill-navbar.tsx
+++ src/components/hill-navbar/hill-navbar.tsx
@@ -18,7 +18,7 @@ export class HillNavbar {
       <Host>
         <nav id="hill-navbar">
           <div id="hill-navbar__home"><a href={`${this.homeLinkUrl}`}><img class="home-img" src={`${this.homeLinkImageUrl}`} /><span class="home-name">{this.homeLinkText}</span></a></div>
-          <div class="hill-navbar__list-wrapper">
+          <div id="hill-navbar__list-wrapper">
             <ul id="hill-navbar__list">
                 {this.menuList.map(text => <li class="list-item" id={`hill-navbar__${text.toLowerCase()}`}><a href={`/${text.toLowerCase()}`}>{text}</a></li>)}
             </ul>
