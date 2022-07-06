98cabe15b32d3e9c1b3887c1cad1d832e1776b04
KHCode <hillkri@oregonstate.edu>
2022-07-05 17:01:00 -0700

    add layout styles

--- src/components/hill-navbar/hill-navbar.tsx
+++ src/components/hill-navbar/hill-navbar.tsx
@@ -9,15 +9,20 @@ export class HillNavbar {
   @Prop() bgColor;
   @Prop() homeLinkText: string = 'Kris Hill';
   @Prop() homeLinkUrl: string = '/';
-  @Prop() homeLinkImageUrl: string = './images/Startup.png';
+  @Prop() homeLinkImageUrl: string = '../../assets/pexels-emiliano-arano-2127969.jpg';
   @Prop() menuList: Array<string> = ['About', 'Resume', 'Play', 'Blog'];
-
+  homeImg: string = "src\assets\pexels-emiliano-arano-2127969.jpg";
   
   render() {
     return (
       <Host>
         <nav id="hill-navbar">
-          <div id="hill-navbar__home"><a href={`${this.homeLinkUrl}`}><img class="home-img" src={`${this.homeLinkImageUrl}`} /><span class="home-name">{this.homeLinkText}</span></a></div>
+          <div id="hill-navbar__home">
+            <a href={this.homeLinkUrl}>
+              <img class="home-img" src={this.homeLinkImageUrl} />
+              <span class="home-name">{this.homeLinkText}</span>
+            </a>
+          </div>
           <div id="hill-navbar__list-wrapper">
             <ul id="hill-navbar__list">
                 {this.menuList.map(text => <li class="list-item" id={`hill-navbar__${text.toLowerCase()}`}><a href={`/${text.toLowerCase()}`}>{text}</a></li>)}
